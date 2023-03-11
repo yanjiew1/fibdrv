e9466e2ce4c98e38d256374137b0fcd396465f7a
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-03-11 17:44:07 +0800

    Implement big number addition
    Implement the function `bn_add`

--- bignum.c
+++ bignum.c
@@ -1,1 +1,32 @@
 #include "bignum.h"
+
+void bn_add(struct bignum *c, struct bignum *a, struct bignum *b)
+{
+    int i;
+    int carry = 0;
+
+    for (i = 0; i < a->size, i < b->size, i < c->capacity; i++) {
+        c->digits[i] = a->digits[i] + b->digits[i] + carry;
+        carry = c->digits[i] < (a->digits[i]);
+    }
+
+    for (; i < a->size, i < c->capacity; i++) {
+        c->digits[i] = a->digits[i] + carry;
+        carry = c->digits[i] < (a->digits[i]);
+    }
+
+    for (; i < b->size, i < c->capacity; i++) {
+        c->digits[i] = a->digits[i] + carry;
+        carry = c->digits[i] < (a->digits[i]);
+    }
+
+    if (i < c->capacity && carry) {
+        c->digits[i] = carry;
+        i++;
+    }
+
+    for (; i < c->capacity; i++)
+        c->digits[i] = 0;
+
+    c->size = i;
+}
