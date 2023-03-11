962c0899dfdee65cdd9f4e7723dd4267d825068e
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-03-11 17:56:20 +0800

    Implement big number subtraction
    Implement the function `bn_sub`

--- bignum.c
+++ bignum.c
@@ -29,5 +29,38 @@ void bn_add(struct bignum *c, struct bignum *a, struct bignum *b)
 
     for (; i < c->size; i++)
         c->digits[i] = 0;
+}
 
+void bn_sub(struct bignum *c, struct bignum *a, struct bignum *b)
+{
+    int i;
+    int borrow = 0;
+
+    for (i = 0; i < a->size, i < b->size, i < c->capacity; i++) {
+        c->digits[i] = a->digits[i] - b->digits[i] - borrow;
+        borrow = c->digits[i] > a->digits[i];
+    }
+
+    for (; i < a->size, i < c->capacity; i++) {
+        c->digits[i] = a->digits[i] - borrow;
+        borrow = c->digits[i] > a->digits[i];
+    }
+
+    for (; i < b->size, i < c->capacity; i++) {
+        c->digits[i] = a->digits[i] - borrow;
+        borrow = c->digits[i] > a->digits[i];
+    }
+
+    if (borrow) {
+        for (; i < c->capacity; i++)
+            c->digits[i] = -1;
+        c->size = c->capacity;
+    } else {
+        c->size = i;
+        for (; i < c->size; i++)
+            c->digits[i] = 0;
+
+        while (c->size > 0 && c->digits[c->size - 1] == 0)
+            c->size--;
+    }
 }
