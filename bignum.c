98254d8a00de5903a55ba1007d32afacd1490e24
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-03-11 17:49:11 +0800

    Remove unnecessary parentheses

--- bignum.c
+++ bignum.c
@@ -7,17 +7,17 @@ void bn_add(struct bignum *c, struct bignum *a, struct bignum *b)
 
     for (i = 0; i < a->size, i < b->size, i < c->capacity; i++) {
         c->digits[i] = a->digits[i] + b->digits[i] + carry;
-        carry = c->digits[i] < (a->digits[i]);
+        carry = c->digits[i] < a->digits[i];
     }
 
     for (; i < a->size, i < c->capacity; i++) {
         c->digits[i] = a->digits[i] + carry;
-        carry = c->digits[i] < (a->digits[i]);
+        carry = c->digits[i] < a->digits[i];
     }
 
     for (; i < b->size, i < c->capacity; i++) {
         c->digits[i] = a->digits[i] + carry;
-        carry = c->digits[i] < (a->digits[i]);
+        carry = c->digits[i] < a->digits[i];
     }
 
     if (i < c->capacity && carry) {
